2cfcae464740498e153565dda83d308912fef2b7
Colton Carls <cwc74@scarletmail.rutgers.edu>
2020-10-30 15:08:14 -0400

    patched navbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { BrowserRouter, NavLink, Switch, Route, useLocation } from 'react-router-dom';
 import './App.css';
-import * as ReactBootStrap from "react-bootstrap";
-import Login from "./Components/login.js";
-import Features from "./Components/Features.js";
-import Pricing from "./Components/Pricing.js";
-import NavBar from "./Components/Navbar.js"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
 
 function App() {
+
+  const navbarLinks = useRef(null);
+  const handleNavbarButton = (e) => {
+    navbarLinks.current.classList.toggle('menu-collapse');
+  };
+
+  const hideNavMenu = () => {
+    if (!navbarLinks.current.classList.contains('menu-collapse')) {
+      navbarLinks.current.classList.add('menu-collapse');
+    }
+  }
+
   return (
     <div className="App">
-        <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/features" component={Features}>
-              <Features />
-          </Route>
-          <Route path="/pricing" component={Pricing}>
-            <Pricing />
-          </Route>
-          <Route path="/login" component={Login}>
-            <Login />
-          </Route>
-        </Switch>
-    </Router>
+      <BrowserRouter>
+        <nav className='navbar'>
+          <div className='navbar-container'>
+            <a href="/Home" className='brand-title'>Cyprus</a>
+            <button onClick={(e) => { handleNavbarButton(e); }} className='navbar-toggler'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div ref={navbarLinks} className='navbar-links menu-collapse'>
+              <ul className='links-list'>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/Home'>Home</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/About'>About</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/Login'>Login</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className='container'>
+          <AllRoutes hideMenu={() => { hideNavMenu(); }}></AllRoutes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
+function AllRoutes({ hideMenu }) {
+
+  let location = useLocation();
+  useEffect(() => {
+    hideMenu();
+  }, [location]);
+
+  return (
+    <Switch>
+      <Route path="/about" component={About}>
+      </Route>
+      <Route path="/login" component={Login}>
+      </Route>
+      <Route path="/" component={Home}>
+      </Route>
+    </Switch>
+  );
+}
+
+function Home() {
+  return (
+    <h1>Home</h1>
+  );
+}
+
+function About() {
+  return (
+    <h1>About</h1>
+  );
+}
+
+function Login() {
+  return (
+    <h1>Login</h1>
+  );
+}
 export default App;
